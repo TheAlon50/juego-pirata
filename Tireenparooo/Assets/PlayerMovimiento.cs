c53d73a2182d5a29cce8f5658fdb03b34105dfb3
Alonso <jesusarr590@gmail.com>
2025-09-23 02:30:17 -0700

    movimiento en prueba

--- Tireenparooo/Assets/PlayerMovimiento.cs
+++ Tireenparooo/Assets/PlayerMovimiento.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovimiento : MonoBehaviour
 {
-    public float movimiento = 5;
+    public float movimiento = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +12,11 @@ public class PlayerMovimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float velocidadX = Input.GetAxis("Horizontal");
+
+        Vector3 posicion = transform.position;
+
+        transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
+
     }
 }
